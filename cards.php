ba5bbe282a465a5897ed0ef4ee81df837fd1ef15
Viktor Leonets <mail@leonec.com>
2012-07-08 20:43:14 +0300

    update

---
+++ cards.php
@@ -0,0 +1,8 @@
+<?php
+require_once("./classes/startup.php");
+require_once("./classes/config.php");
+require_once("./classes/cards.php");
+
+$cards = new cards();
+echo 'hi';
+?>
---
+++ classes/cards.php
@@ -0,0 +1,13 @@
+<?php
+class cards extends datebase{
+
+	private $limit;
+	private $count;
+	
+	public $cards = false;
+	public function getCards(){
+	
+		return $this->cards;
+	}
+}
+?>
